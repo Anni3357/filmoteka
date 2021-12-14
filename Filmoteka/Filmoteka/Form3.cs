39e3637fa8ad4df099c58a8ea1aa25565cfe2c94
superperchik <60111258+superperchik@users.noreply.github.com>
2021-12-14 17:00:46 +0300

    какие-то изменения

--- Filmoteka/Filmoteka/Form3.cs
+++ Filmoteka/Filmoteka/Form3.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace Filmoteka
 {
@@ -19,13 +21,36 @@ namespace Filmoteka
         
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-           
+          /*  string mainconn = ConfigurationManager.ConnectionStrings["Filmoteka"].ConnectionString;
+            SqlConnection sqlconn = new SqlConnection(mainconn);
+            string sqlquery = "Select * from [dbo].[FilmDB] where director"+comboBox1.Text.ToString()+"^";
+            SqlCommand sqlcomn = new SqlCommand(sqlquery, sqlconn);
+            sqlconn.Open();
+            SqlDataAdapter sdr = new SqlDataAdapter(sqlcomn);
+            DataTable dt = new DataTable();
+            sdr.Fill(dt);
+            filmsDataGridView.DataSource = dt;
+            sqlconn.Close(); */
+
         }
 
         private void Form3_Load(object sender, EventArgs e)
         {
+           /* string mainconn = ConfigurationManager.ConnectionStrings["Filmoteka"].ConnectionString;
+            SqlConnection sqlconn = new SqlConnection(mainconn);
+            string sqlquery = "Select director from [dbo].[FilmDB]";
+            SqlCommand sqlcomn = new SqlCommand(sqlquery, sqlconn);
+            sqlconn.Open();
+            SqlDataAdapter sdr = new SqlDataAdapter(sqlcomn);
+            DataTable dt = new DataTable();
+            sdr.Fill(dt);
+            comboBox1.DisplayMember = "director";
+            comboBox1.DataSource = dt;
+            sqlconn.Close(); */
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "dataSet1.Films". При необходимости она может быть перемещена или удалена.
+            //this.filmsTableAdapter.Fill(this.dataSet1.Films);
             // TODO: данная строка кода позволяет загрузить данные в таблицу "dataSet1.Director". При необходимости она может быть перемещена или удалена.
-            this.directorTableAdapter.Fill(this.dataSet1.Director);
+            //this.directorTableAdapter.Fill(this.dataSet1.Director);
             
         }
     }
